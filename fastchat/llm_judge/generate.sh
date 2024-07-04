99ed02f56c80bfd189d78d9eed50248b486f2a78
ErlebnisW <787368748w@gmail.com>
2024-07-05 00:06:44 +0800

    add sh for generating answer

--- fastchat/llm_judge/generate.sh
+++ fastchat/llm_judge/generate.sh
@@ -1,4 +1,5 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 python gen_model_answer.py \
-        --model-path /data1/WM_workspace/MDSPO/output/qwen2-1.5b_alpaca_sft \
-        --model-id qwen2_alpaca_sft \
+        --model-path /data1/WM_workspace/MDSPO/output/qwen2-alpaca_sp_3/sp_3 \
+        --model-id qwen2_sp_3 \
         --num-gpus-total 8
